388a5a140558efe79601d5a155ccaf05855fafe7
Ronit Pradhan <i.ronnieful@gmail.com>
2022-08-01 19:48:43 +0100

    Added Heroku deployment support

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar build/libs/xchangecurrency-0.0.1-SNAPSHOT.jar
--- src/test/java/com/xchangecurrency/CurrencyExchangeApiApplicationTests.java
+++ src/test/java/com/xchangecurrency/CurrencyExchangeApiApplicationTests.java
@@ -1,13 +1,6 @@
 package com.xchangecurrency;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+//@SpringBootTest
 class CurrencyExchangeApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
